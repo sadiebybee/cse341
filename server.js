27593b9ecf6c4cee448777d5ec2d4bd709b3ccec
sadiebybee <byb22004@byui.edu>
2025-05-15 15:07:14 -0600

    update

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const mongodb = require("./db/connect");
 const routes = require("./routes");
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
 app.use('/', routes);
 
 mongodb.initDb((err, mongodb) => {
